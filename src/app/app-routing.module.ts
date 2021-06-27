3c291490a1a9e99d5cb1284a42bf1d4bbbb0ae4c
Renato Pereira <pereirarenato21@gmail.com>
2021-06-27 12:27:13 -0300

    Navbar finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: AppComponent
-  }
-];
+const routes: Routes = []
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
