dbaa76f0b941b128e2648166e1daabb50e9ae82e
renato3x <pereirarenato21@gmail.com>
2021-08-23 20:20:34 -0300

    post.module placed in the same folder as the post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
-import { PostModule } from './post/post.module';
+import { PostModule } from './components/post/post.module';
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/post/post.module.ts
+++ src/app/components/post/post.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PostComponent } from '../components/post/post.component';
-import { PostHeaderComponent } from '../components/post/post-header/post-header.component';
-import { PostImageComponent } from '../components/post/post-image/post-image.component';
-import { PostFooterComponent } from '../components/post/post-footer/post-footer.component';
+import { PostComponent } from './post.component';
+import { PostHeaderComponent } from './post-header/post-header.component';
+import { PostImageComponent } from './post-image/post-image.component';
+import { PostFooterComponent } from './post-footer/post-footer.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
